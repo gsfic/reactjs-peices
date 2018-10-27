2ee91a8b2f85d18bf6923fc107765c75f4d65156
Amine Tabou <amin.tabou@gmail.com>
2018-10-27 19:51:58 +0100

    extra paragraph

---
+++ src/App1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App1 extends React.Component {
+
+    
+    render() {
+        return (
+            <div>
+                I am JavaScript
+            </div>
+        );
+    }
+}
+
+export default App1;
